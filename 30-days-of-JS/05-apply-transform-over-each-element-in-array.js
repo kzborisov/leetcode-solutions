01a26f8a924cf72d57ba54f73641c140469f8637
Kristian Borisov <kborisov@lely.com>
2023-07-06 10:24:10 +0300

    Solved day 5 of 30 days of JS without map

--- 30-days-of-JS/05-apply-transform-over-each-element-in-array.js
+++ 30-days-of-JS/05-apply-transform-over-each-element-in-array.js
@@ -4,7 +4,12 @@
  * @return {number[]}
  */
 const map = function (arr, fn) {
-  return arr.map((el, idx) => fn(el, idx));
+  const result = new Array(arr.length);
+  for (let idx = 0; idx < arr.length; idx++) {
+    result[idx] = fn(arr[idx], idx);
+  }
+  return result;
+  // return arr.map((el, idx) => fn(el, idx));
 };
 
 const arr = [1, 2, 3];
@@ -19,13 +24,13 @@ const fn2 = function plusI(n, i) {
   return n + i;
 };
 
-const newArray2 = map(arr2, fn2);
-console.log(newArray2); // [1,3,5]
+const result2 = map(arr2, fn2);
+console.log(result2); // [1,3,5]
 
 const arr3 = [10, 20, 30];
 const fn3 = function constant() {
   return 42;
 };
 
-const newArray3 = map(arr3, fn3);
-console.log(newArray3); // [42, 42, 42]
+const result3 = map(arr3, fn3);
+console.log(result3); // [42, 42, 42]
