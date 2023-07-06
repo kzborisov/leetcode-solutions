38ed6f3628e8ff5b6e0eb7a4f9714be34027d116
Kristian Borisov <kborisov@lely.com>
2023-07-06 09:49:20 +0300

    Solved day 6 of 30 days of JS

---
+++ 30-days-of-JS/06-filter-elements-from-array.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+const filter = function (arr, fn) {
+  return arr.filter((el, idx) => fn(el, idx));
+};
+
+console.log(
+  filter([0, 10, 20, 30], function greaterThan10(n) {
+    return n > 10;
+  })
+); // [20, 30]
+
+console.log(
+  filter([1, 2, 3], function firstIndex(n, i) {
+    return i === 0;
+  })
+); // [1]
+
+console.log(
+  filter([-2, -1, 0, 1, 2], function plusOne(n) {
+    return n + 1;
+  })
+); // [-2,0,1,2]
