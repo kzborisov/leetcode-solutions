ebb66ff1e23bb49af1b97de583632bb8e065e409
Kristian Borisov <kborisov@lely.com>
2023-07-07 13:40:30 +0300

    Solved day 9 of 30 days of JS

---
+++ 30-days-of-JS/09-return-lenght-of-arguments-passed.js
@@ -0,0 +1,9 @@
+/**
+ * @return {number}
+ */
+var argumentsLength = function (...args) {
+  return args.length;
+};
+
+console.log(argumentsLength(5)); // 1
+console.log(argumentsLength({}, null, "3")); // 3
